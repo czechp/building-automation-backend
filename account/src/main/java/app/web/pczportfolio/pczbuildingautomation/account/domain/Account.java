ee5d1d9079164559722d35f0219887e851a48c8a
czechp <webcoderc@gmail.com>
2022-06-13 14:27:24 +0200

    Structure of account moduel

---
+++ account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/domain/Account.java
@@ -0,0 +1,4 @@
+package app.web.pczportfolio.pczbuildingautomation.account.domain;
+
+public class Account {
+}
---
+++ common/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/dto/AccountQueryDto.java
@@ -0,0 +1,4 @@
+package app.web.pczportfolio.pczbuildingautomation.account.dto;
+
+public class AccountQueryDto {
+}
