c090b2cbd0a84f06ba5c551af53e9e4189881087
czechp <webcoderc@gmail.com>
2022-06-13 16:28:02 +0200

    Define PasswordEncoder @Bean

--- common/src/main/java/app/web/pczportfolio/pczbuildingautomation/configuration/SpringSecurityConfiguration.java
+++ common/src/main/java/app/web/pczportfolio/pczbuildingautomation/configuration/SpringSecurityConfiguration.java
@@ -1,2 +1,14 @@
-package app.web.pczportfolio.pczbuildingautomation.configuration;class SpringSecurityConfiguration {
+package app.web.pczportfolio.pczbuildingautomation.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+class SpringSecurityConfiguration {
+    @Bean
+    PasswordEncoder getBcryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
