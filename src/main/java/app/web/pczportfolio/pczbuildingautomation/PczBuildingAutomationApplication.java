cb3ff994fba3b65985ebcf1a5717fe08ff7ec16a
czechp <webcoderc@gmail.com>
2022-06-13 10:48:56 +0200

    Init commit

---
+++ src/main/java/app/web/pczportfolio/pczbuildingautomation/PczBuildingAutomationApplication.java
@@ -0,0 +1,13 @@
+package app.web.pczportfolio.pczbuildingautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PczBuildingAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PczBuildingAutomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/web/pczportfolio/pczbuildingautomation/PczBuildingAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package app.web.pczportfolio.pczbuildingautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PczBuildingAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
