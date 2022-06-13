fa9f736c430747e4f908c1bedbea9d165371f2c3
czechp <webcoderc@gmail.com>
2022-06-13 16:36:48 +0200

    Set TODO

--- account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/application/service/AccountCommandService.java
+++ account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/application/service/AccountCommandService.java
@@ -1,12 +1,14 @@
 package app.web.pczportfolio.pczbuildingautomation.account.application.service;
 
+import app.web.pczportfolio.pczbuildingautomation.account.application.useCase.AccountCreateUseCase;
 import lombok.AllArgsConstructor;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
+//TODO: implement it
 @Service
 @AllArgsConstructor
-class AccountCommandService {
+class AccountCommandService implements AccountCreateUseCase {
 
 }
