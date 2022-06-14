64260a45b1dec06b472bbc14ced80ff361e89f00
czechp <webcoderc@gmail.com>
2022-06-14 22:53:01 +0200

    Simple security configuration to allows request for testing

--- common/src/main/java/app/web/pczportfolio/pczbuildingautomation/configuration/SpringSecurityConfiguration.java
+++ common/src/main/java/app/web/pczportfolio/pczbuildingautomation/configuration/SpringSecurityConfiguration.java
@@ -2,13 +2,30 @@ package app.web.pczportfolio.pczbuildingautomation.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 class SpringSecurityConfiguration {
     @Bean
     PasswordEncoder getBcryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain getSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests(
+                        (authorization) -> authorization
+                                .antMatchers("/**").permitAll()
+
+                )
+                .csrf().disable()
+                .httpBasic();
+
+        return httpSecurity.build();
     }
 }
