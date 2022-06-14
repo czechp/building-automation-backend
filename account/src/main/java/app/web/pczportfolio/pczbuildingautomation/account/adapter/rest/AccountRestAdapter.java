f48eb9e00f28fd72dbf89f0589f39a9fe5d10603
czechp <webcoderc@gmail.com>
2022-06-14 22:53:47 +0200

    Endpoint to handler request to creating a new account

--- account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/rest/AccountRestAdapter.java
+++ account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/rest/AccountRestAdapter.java
@@ -1,13 +1,22 @@
 package app.web.pczportfolio.pczbuildingautomation.account.adapter.rest;
 
+import app.web.pczportfolio.pczbuildingautomation.account.application.useCase.AccountCreateUseCase;
+import app.web.pczportfolio.pczbuildingautomation.account.dto.AccountCommandDto;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/accounts")
 @CrossOrigin("*")
 @AllArgsConstructor
 class AccountRestAdapter {
+    private final AccountCreateUseCase accountCreateUseCase;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    void createAccount(@RequestBody @Valid AccountCommandDto accountCommandDto) {
+        accountCreateUseCase.createAccount(accountCommandDto);
+    }
 }
