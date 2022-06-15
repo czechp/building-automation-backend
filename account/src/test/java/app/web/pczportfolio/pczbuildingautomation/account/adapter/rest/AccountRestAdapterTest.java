37f9bf9de03062e85e8d6a45359587aae20a175d
czechp <webcoderc@gmail.com>
2022-06-15 12:37:39 +0200

    Test for AccountRestAdapter.createAccount

--- account/src/test/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/rest/AccountRestAdapterTest.java
+++ account/src/test/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/rest/AccountRestAdapterTest.java
@@ -2,23 +2,33 @@ package app.web.pczportfolio.pczbuildingautomation.account.adapter.rest;
 
 import app.web.pczportfolio.pczbuildingautomation.account.application.port.AccountCreatePort;
 import app.web.pczportfolio.pczbuildingautomation.account.application.port.AccountFindByUsernameOrEmailPort;
-import app.web.pczportfolio.pczbuildingautomation.account.application.useCase.AccountCreateUseCase;
+import app.web.pczportfolio.pczbuildingautomation.account.domain.Account;
+import app.web.pczportfolio.pczbuildingautomation.account.dto.AccountCommandDto;
 import app.web.pczportfolio.pczbuildingautomation.configuration.HttpExceptionHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.anyString;
 
 @SpringJUnitConfig(AccountRestAdapterTest.TestConfiguration.class)
 class AccountRestAdapterTest {
+    private static final String URL = "/api/accounts";
 
-    @MockBean
-    AccountCreateUseCase accountCreateUseCase;
     @MockBean
     AccountFindByUsernameOrEmailPort accountFindByUsernameOrEmailPort;
     @MockBean
@@ -29,6 +39,7 @@ class AccountRestAdapterTest {
 
     MockMvc mockMvc;
 
+    ObjectMapper objectMapper;
 
     @BeforeEach
     void init() {
@@ -36,14 +47,72 @@ class AccountRestAdapterTest {
                 .standaloneSetup(accountRestAdapter)
                 .setControllerAdvice(new HttpExceptionHandler())
                 .build();
+        this.objectMapper = new ObjectMapper();
     }
 
     @Test
-    void createAccountTest() {
+    void createAccountTest() throws Exception {
         //given
+        AccountCommandDto accountCommandDto = new AccountCommandDto(
+                "someUsername",
+                "some@gmail.com",
+                "somePassword",
+                "somePassword");
+        String requestBody = objectMapper.writeValueAsString(accountCommandDto);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody);
         //when
+        Mockito.when(accountFindByUsernameOrEmailPort
+                        .findByUsernameOrEmail(anyString(), anyString()))
+                .thenReturn(Optional.empty());
         //then
+        mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isCreated());
     }
+
+    @Test
+    void createAccountPasswordNotEqualsTest() throws Exception {
+        //given
+        AccountCommandDto accountCommandDto = new AccountCommandDto(
+                "someUsername",
+                "some@gmail.com",
+                "somePassword",
+                "somePassword123");
+        String requestBody = objectMapper.writeValueAsString(accountCommandDto);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody);
+        //when
+        Mockito.when(accountFindByUsernameOrEmailPort
+                        .findByUsernameOrEmail(anyString(), anyString()))
+                .thenReturn(Optional.empty());
+        //then
+        mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void createAccountPasswordAccountAlreadyExistsTest() throws Exception {
+        //given
+        AccountCommandDto accountCommandDto = new AccountCommandDto(
+                "someUsername",
+                "some@gmail.com",
+                "somePassword",
+                "somePassword");
+        String requestBody = objectMapper.writeValueAsString(accountCommandDto);
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.post(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody);
+        //when
+        Mockito.when(accountFindByUsernameOrEmailPort
+                        .findByUsernameOrEmail(anyString(), anyString()))
+                .thenReturn(Optional.of(new Account()));
+        //then
+        mockMvc.perform(requestBuilder)
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
 
     @Configuration
     @ComponentScan("app.web.pczportfolio.pczbuildingautomation.account")
