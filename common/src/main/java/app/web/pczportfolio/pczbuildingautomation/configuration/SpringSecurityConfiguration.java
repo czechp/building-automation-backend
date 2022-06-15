1059a69c889592a169b68574f7c5612472e66371
czechp <webcoderc@gmail.com>
2022-06-15 12:40:11 +0200

    OPTION request for all
    Allow to sending option request for every user to check does any
    endpoint exists.
    JS http requests libs make it before every request

--- common/src/main/java/app/web/pczportfolio/pczbuildingautomation/configuration/SpringSecurityConfiguration.java
+++ common/src/main/java/app/web/pczportfolio/pczbuildingautomation/configuration/SpringSecurityConfiguration.java
@@ -2,8 +2,8 @@ package app.web.pczportfolio.pczbuildingautomation.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +21,7 @@ class SpringSecurityConfiguration {
                 .authorizeRequests(
                         (authorization) -> authorization
                                 .antMatchers("/**").permitAll()
+                                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 
                 )
                 .csrf().disable()
