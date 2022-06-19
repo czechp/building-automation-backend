e9544b813bfb2dad36bed668b9bdee102fd63dd0
czechp <webcoderc@gmail.com>
2022-06-19 22:57:06 +0200

    Merge all persistence ports in one @service class

---
+++ account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/persistence/AccountPersistenceAdapter.java
@@ -0,0 +1,49 @@
+package app.web.pczportfolio.pczbuildingautomation.account.adapter.persistence;
+
+import app.web.pczportfolio.pczbuildingautomation.account.application.port.*;
+import app.web.pczportfolio.pczbuildingautomation.account.domain.Account;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+class AccountPersistenceAdapter implements
+        AccountPortDelete,
+        AccountPortFindById,
+        AccountPortFindByUsername,
+        AccountPortFindByUsernameOrEmail,
+        AccountPortSave {
+    private final AccountJpaRepository accountJpaRepository;
+
+    @Override
+    public void deleteAccount(Account account) {
+        accountJpaRepository.delete(AccountEntityMapper.toEntity(account));
+    }
+
+    @Override
+    public Optional<Account> findAccountById(long id) {
+        return accountJpaRepository.findById(id)
+                .map(AccountEntityMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Account> findAccountByUsername(String username) {
+        return accountJpaRepository.findByUsername(username)
+                .map(AccountEntityMapper::toDomain);
+    }
+
+    @Override
+    public Optional<Account> findAccountByUsernameOrEmail(String username, String email) {
+        return accountJpaRepository.findByUsernameOrEmail(username, email)
+                .map(AccountEntityMapper::toDomain);
+    }
+
+    @Override
+    public Account saveAccount(Account account) {
+        AccountEntity accountEntity = AccountEntityMapper.toEntity(account);
+        return AccountEntityMapper.toDomain(accountJpaRepository.save(accountEntity));
+    }
+
+}
