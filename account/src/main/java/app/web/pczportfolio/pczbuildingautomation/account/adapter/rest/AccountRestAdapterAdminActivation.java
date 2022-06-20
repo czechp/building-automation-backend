e99084960fd5784ff7b43e972f7044ea73e6ef89
czechp <webcoderc@gmail.com>
2022-06-20 12:51:47 +0200

    Endpoint to activate account by admin with tests

---
+++ account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/rest/AccountRestAdapterAdminActivation.java
@@ -0,0 +1,24 @@
+package app.web.pczportfolio.pczbuildingautomation.account.adapter.rest;
+
+import app.web.pczportfolio.pczbuildingautomation.account.application.useCase.AccountUseCaseAdminActivation;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/accounts")
+class AccountRestAdapterAdminActivation {
+    private final AccountUseCaseAdminActivation accountUseCaseAdminActivation;
+
+    @Secured({"ROLE_ADMIN"})
+    @PatchMapping("/admin-activation/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void accountAdminActivation(
+            @PathVariable(name = "id") final long id,
+            @RequestParam(name = "activation") final boolean activation
+    ) {
+        accountUseCaseAdminActivation.accountAdminActivation(id, activation);
+    }
+}
