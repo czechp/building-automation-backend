05dbde6c5ca26457757f135bf128440fc46be366
czechp <webcoderc@gmail.com>
2022-07-01 14:52:23 +0200

    Endpoint to handle switch device queries

---
+++ switch-device/src/main/java/app/web/pczportfolio/pczbuildingautomation/switchDevice/adapter/rest/SwitchDeviceRestAdapterQuery.java
@@ -0,0 +1,39 @@
+package app.web.pczportfolio.pczbuildingautomation.switchDevice.adapter.rest;
+
+
+import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.dto.SwitchDeviceQueryDto;
+import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.query.SwitchDeviceQuery;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/switch-devices")
+@CrossOrigin("*")
+@AllArgsConstructor
+class SwitchDeviceRestAdapterQuery {
+    private final SwitchDeviceQuery switchDeviceQuery;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Secured({"ROLE_ADMIN"})
+    List<SwitchDeviceQueryDto> findAllSwitchDevices(Pageable pageable) {
+        return switchDeviceQuery.findAllSwitchDevices(pageable);
+    }
+
+    @GetMapping("/account")
+    @ResponseStatus(HttpStatus.OK)
+    List<SwitchDeviceQueryDto> findAllSwitchDevicesByCurrentUser(Pageable pageable) {
+        return switchDeviceQuery.findAllSwitchDevicesByCurrentUser(pageable);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    SwitchDeviceQueryDto findSwitchDeviceBtId(@PathVariable(name = "id") long switchDeviceId) {
+        return switchDeviceQuery.findSwitchDeviceById(switchDeviceId);
+    }
+}
