9ff4e5f05001edeb210cc1b45c99d12866899bf8
czechp <webcoderc@gmail.com>
2022-07-01 21:31:55 +0200

    Port to delete switch device with implementation

---
+++ switch-device/src/main/java/app/web/pczportfolio/pczbuildingautomation/switchDevice/application/port/SwitchDevicePortDelete.java
@@ -0,0 +1,7 @@
+package app.web.pczportfolio.pczbuildingautomation.switchDevice.application.port;
+
+import app.web.pczportfolio.pczbuildingautomation.switchDevice.domain.SwitchDevice;
+
+public interface SwitchDevicePortDelete {
+    void deleteSwitchDevice(SwitchDevice switchDevice);
+}
