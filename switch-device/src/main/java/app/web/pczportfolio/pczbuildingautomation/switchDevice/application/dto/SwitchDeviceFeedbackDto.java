b6c43e9714f6c870b7c8a93c835eaa6c50fa86f8
czechp <webcoderc@gmail.com>
2022-07-02 20:17:36 +0200

    Dto to supply feedback from switch device

---
+++ switch-device/src/main/java/app/web/pczportfolio/pczbuildingautomation/switchDevice/application/dto/SwitchDeviceFeedbackDto.java
@@ -0,0 +1,13 @@
+package app.web.pczportfolio.pczbuildingautomation.switchDevice.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SwitchDeviceFeedbackDto {
+    private long id;
+    private long currentState;
+}
