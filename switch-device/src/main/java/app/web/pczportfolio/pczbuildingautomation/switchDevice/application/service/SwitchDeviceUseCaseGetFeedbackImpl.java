e9556aa52d219b0ed05ee3c340febfb96cf2dde8
czechp <webcoderc@gmail.com>
2022-07-03 14:51:06 +0200

    Implementation of SwitchDeviceUseCaseGetFeedaback

--- switch-device/src/main/java/app/web/pczportfolio/pczbuildingautomation/switchDevice/application/service/SwitchDeviceUseCaseGetFeedbackImpl.java
+++ switch-device/src/main/java/app/web/pczportfolio/pczbuildingautomation/switchDevice/application/service/SwitchDeviceUseCaseGetFeedbackImpl.java
@@ -1,19 +1,32 @@
 package app.web.pczportfolio.pczbuildingautomation.switchDevice.application.service;
 
+import app.web.pczportfolio.pczbuildingautomation.exception.NotFoundException;
 import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.dto.SwitchDeviceFeedbackDto;
 import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.port.SwitchDevicePortFindById;
+import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.port.SwitchDevicePortSave;
 import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.useCase.SwitchDeviceUseCaseGetFeedback;
 import app.web.pczportfolio.pczbuildingautomation.switchDevice.domain.SwitchDevice;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @AllArgsConstructor
 class SwitchDeviceUseCaseGetFeedbackImpl implements SwitchDeviceUseCaseGetFeedback {
     private final SwitchDevicePortFindById switchDevicePortFindById;
+    private final SwitchDevicePortSave switchDevicePortSave;
 
+    @Transactional
     @Override
     public SwitchDevice receiveFeedbackFromDevice(SwitchDeviceFeedbackDto switchDeviceFeedbackDto) {
-        return null;
+        final var  switchDeviceToUpdate = findSwitchDeviceOrThrowException(switchDeviceFeedbackDto);
+        switchDeviceToUpdate.receiveFeedback(switchDeviceFeedbackDto);
+        switchDevicePortSave.saveSwitchDevice(switchDeviceToUpdate);
+        return switchDeviceToUpdate;
+    }
+
+    private SwitchDevice findSwitchDeviceOrThrowException(SwitchDeviceFeedbackDto switchDeviceFeedbackDto) {
+        return switchDevicePortFindById.findSwitchDeviceById(switchDeviceFeedbackDto.getId())
+                .orElseThrow(() -> new NotFoundException("Switch device with id: " + switchDeviceFeedbackDto.getId() + " does not exist"));
     }
 }
