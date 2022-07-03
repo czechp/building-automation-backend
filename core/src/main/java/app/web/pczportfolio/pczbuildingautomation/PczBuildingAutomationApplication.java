8f99405283ff5731672b92412e4c4b70a998ba5f
czechp <webcoderc@gmail.com>
2022-07-03 19:52:29 +0200

    Enable scheduling and set in CRON checking device errors for all switch devices

--- core/src/main/java/app/web/pczportfolio/pczbuildingautomation/PczBuildingAutomationApplication.java
+++ core/src/main/java/app/web/pczportfolio/pczbuildingautomation/PczBuildingAutomationApplication.java
@@ -2,8 +2,10 @@ package app.web.pczportfolio.pczbuildingautomation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PczBuildingAutomationApplication {
 
     public static void main(String[] args) {
---
+++ switch-device/src/main/java/app/web/pczportfolio/pczbuildingautomation/switchDevice/adapter/SwitchDeviceCronAdapterCheckError.java
@@ -0,0 +1,33 @@
+package app.web.pczportfolio.pczbuildingautomation.switchDevice.adapter;
+
+
+import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.port.SwitchDevicePortFindAll;
+import app.web.pczportfolio.pczbuildingautomation.switchDevice.application.useCase.SwitchDeviceUseCaseCheckError;
+import app.web.pczportfolio.pczbuildingautomation.switchDevice.domain.SwitchDevice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+class SwitchDeviceCronAdapterCheckError {
+    private final SwitchDevicePortFindAll switchDevicePortFindAll;
+    private final SwitchDeviceUseCaseCheckError switchDeviceUseCaseCheckError;
+
+    public SwitchDeviceCronAdapterCheckError(SwitchDevicePortFindAll switchDevicePortFindAll, SwitchDeviceUseCaseCheckError switchDeviceUseCaseCheckError) {
+        this.switchDevicePortFindAll = switchDevicePortFindAll;
+        this.switchDeviceUseCaseCheckError = switchDeviceUseCaseCheckError;
+    }
+
+    @Scheduled(fixedDelay = 30_000)
+    @Transactional
+    public void checkErrorsForAllDevices() {
+        switchDevicePortFindAll.findAllSwitchDevices(Pageable.unpaged())
+                .stream()
+                .map(SwitchDevice::getId)
+                .forEach(switchDeviceUseCaseCheckError::checkSwitchDeviceError);
+    }
+}
+
