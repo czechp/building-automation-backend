10ca04d1387554cac1e2baef4f6f808dbb79dcb5
czechp <webcoderc@gmail.com>
2022-07-07 13:02:52 +0200

    Location consumer for account delete event

---
+++ location/src/main/java/app/web/pczportfolio/pczbuildingautomation/location/adapter/spring/LocationEventConsumerAdapter.java
@@ -0,0 +1,17 @@
+package app.web.pczportfolio.pczbuildingautomation.location.adapter.spring;
+
+import app.web.pczportfolio.pczbuildingautomation.account.event.AccountDeleteEvent;
+import app.web.pczportfolio.pczbuildingautomation.location.application.useCase.LocationUseCaseDelete;
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+class LocationEventConsumerAdapter implements ApplicationListener<AccountDeleteEvent> {
+    private final LocationUseCaseDelete locationUseCaseDelete;
+
+    @Override
+    public void onApplicationEvent(AccountDeleteEvent accountDeleteEvent) {
+    }
+}
