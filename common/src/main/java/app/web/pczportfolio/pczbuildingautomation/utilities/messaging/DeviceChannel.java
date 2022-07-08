6f1c5c46f1a753c2426908f1452bca844a8b2573
czechp <webcoderc@gmail.com>
2022-07-08 20:40:23 +0200

    Additional method in DeviceChannel

--- common/src/main/java/app/web/pczportfolio/pczbuildingautomation/utilities/messaging/DeviceChannel.java
+++ common/src/main/java/app/web/pczportfolio/pczbuildingautomation/utilities/messaging/DeviceChannel.java
@@ -1,9 +1,17 @@
 package app.web.pczportfolio.pczbuildingautomation.utilities.messaging;
 
-public interface DeviceChannel {
+public interface DeviceChannel <T> {
     long getId();
 
     String getOwner();
 
     String getChannelRootName();
+
+    String getName();
+
+    T getState();
+
+    T getExpectedState();
+
+    String getDeviceTypeName();
 }
