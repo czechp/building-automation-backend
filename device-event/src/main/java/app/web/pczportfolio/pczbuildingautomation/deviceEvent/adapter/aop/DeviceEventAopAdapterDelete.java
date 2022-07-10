4ece2c99ea1419ed9e6cd5d607e5e062ffa9f178
czechp <webcoderc@gmail.com>
2022-07-10 12:01:32 +0200

    Adapter to create delete event

---
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/aop/DeviceEventAopAdapterDelete.java
@@ -0,0 +1,51 @@
+package app.web.pczportfolio.pczbuildingautomation.deviceEvent.adapter.aop;
+
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.useCase.DeviceEventUseCaseCreate;
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.domain.EventType;
+import app.web.pczportfolio.pczbuildingautomation.utilities.messaging.DeviceChannel;
+import lombok.AllArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+@Aspect
+@Component
+@AllArgsConstructor
+class DeviceEventAopAdapterDelete {
+    private final DeviceEventUseCaseCreate deviceEventUseCaseCreate;
+
+    @AfterReturning(
+            value = "@annotation(app.web.pczportfolio.pczbuildingautomation.deviceEvent.annotation.DeleteDeviceEvent)",
+            returning = "deviceChannel"
+    )
+    public void deleteDeviceEventAdvice(JoinPoint joinPoint, Object deviceChannel) {
+        if (deviceChannel instanceof DeviceChannel)
+            createSingleRemoveEvent(deviceChannel);
+        if (deviceChannel instanceof List)
+            createManyRemoveEvent(deviceChannel);
+    }
+
+
+    private void createSingleRemoveEvent(Object deviceChannel) {
+        DeviceEventAopDeviceChannelMapper.castToDeviceChannel(deviceChannel)
+                .ifPresent(this::createDeleteEvent);
+    }
+
+    private void createDeleteEvent(DeviceChannel channel) {
+        deviceEventUseCaseCreate.createDeviceEvent(channel, EventType.DELETE);
+    }
+
+
+    private void createManyRemoveEvent(Object deviceChannel) {
+        final var deviceChannelList = (List<DeviceChannel>) deviceChannel;
+        deviceChannelList.stream()
+                .map(DeviceEventAopDeviceChannelMapper::castToDeviceChannel)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .forEach(this::createDeleteEvent);
+    }
+}
