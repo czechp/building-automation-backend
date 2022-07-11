c69f593004ee0bcd42700db20ca5f8c0ce98c9d9
czechp <webcoderc@gmail.com>
2022-07-11 18:29:08 +0200

    Abstract method in enum to getting fully name

--- device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/persistence/DeviceEventPersistenceAdapter.java
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/persistence/DeviceEventPersistenceAdapter.java
@@ -1,18 +1,31 @@
 package app.web.pczportfolio.pczbuildingautomation.deviceEvent.adapter.persistence;
 
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindAll;
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortSave;
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.domain.DeviceEvent;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
-class DeviceEventPersistenceAdapter implements DeviceEventPortSave {
+class DeviceEventPersistenceAdapter implements DeviceEventPortSave, DeviceEventPortFindAll {
     private final DeviceEventJpaRepository deviceEventJpaRepository;
 
     @Override
     public DeviceEvent save(DeviceEvent deviceEvent) {
         final var deviceEventEntity = DeviceEventEntityMapper.toEntity(deviceEvent);
         return DeviceEventEntityMapper.toDomain(deviceEventJpaRepository.save(deviceEventEntity));
+    }
+
+    @Override
+    public List<DeviceEvent> findAllDeviceEvents(Pageable pageable) {
+        return deviceEventJpaRepository.findAll()
+                .stream()
+                .map(DeviceEventEntityMapper::toDomain)
+                .collect(Collectors.toList());
     }
 }
