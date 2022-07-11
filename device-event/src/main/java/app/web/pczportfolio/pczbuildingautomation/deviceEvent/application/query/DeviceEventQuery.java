036d4c2dd90bdca9e0db4779e4b83790dc6735ee
czechp <webcoderc@gmail.com>
2022-07-11 19:07:18 +0200

    Add id parameter to findById method

--- device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/application/query/DeviceEventQuery.java
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/application/query/DeviceEventQuery.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface DeviceEventQuery {
     List<DeviceEventQueryDto> findAllDeviceEvents(Pageable pageable);
     List<DeviceEventQueryDto> findByCurrentUser(String owner, Pageable pageable);
-    DeviceEventQueryDto findById();
+    DeviceEventQueryDto findById(long deviceEventId);
 }
