3c6429601c19b2f4e8e26ad73d5d84437f05e038
czechp <webcoderc@gmail.com>
2022-07-11 19:07:46 +0200

    EventDeviceQuery implementation: findById

--- device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/application/query/DeviceEventQueryImpl.java
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/application/query/DeviceEventQueryImpl.java
@@ -2,8 +2,10 @@ package app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.query
 
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.dto.DeviceEventQueryDto;
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindAll;
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindById;
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindByOwner;
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindCurrentUser;
+import app.web.pczportfolio.pczbuildingautomation.exception.NotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -17,6 +19,7 @@ class DeviceEventQueryImpl implements DeviceEventQuery {
     private final DeviceEventPortFindAll deviceEventPortFindAll;
     private final DeviceEventPortFindByOwner deviceEventPortFindByOwner;
     private final DeviceEventPortFindCurrentUser deviceEventPortFindCurrentUser;
+    private final DeviceEventPortFindById deviceEventPortFindById;
 
     @Override
     public List<DeviceEventQueryDto> findAllDeviceEvents(Pageable pageable) {
@@ -38,7 +41,9 @@ class DeviceEventQueryImpl implements DeviceEventQuery {
 
     //TODO
     @Override
-    public DeviceEventQueryDto findById() {
-        return null;
+    public DeviceEventQueryDto findById(long deviceEventId) {
+        return deviceEventPortFindById.findDeviceEventById(deviceEventId)
+                .map(DeviceEventQueryDto::create)
+                .orElseThrow(() -> new NotFoundException("Device event with deviceEventId: " + deviceEventId + " does not exist."));
     }
 }
