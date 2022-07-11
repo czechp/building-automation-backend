fdd64ae1215a9ab8a34a40687171b727d3b592a1
czechp <webcoderc@gmail.com>
2022-07-11 19:01:49 +0200

    DeviceEventQuery implementation: findByCurrentUser

--- device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/application/query/DeviceEventQueryImpl.java
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/application/query/DeviceEventQueryImpl.java
@@ -2,6 +2,8 @@ package app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.query
 
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.dto.DeviceEventQueryDto;
 import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindAll;
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindByOwner;
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.port.DeviceEventPortFindCurrentUser;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -13,19 +15,28 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 class DeviceEventQueryImpl implements DeviceEventQuery {
     private final DeviceEventPortFindAll deviceEventPortFindAll;
+    private final DeviceEventPortFindByOwner deviceEventPortFindByOwner;
+    private final DeviceEventPortFindCurrentUser deviceEventPortFindCurrentUser;
 
     @Override
     public List<DeviceEventQueryDto> findAllDeviceEvents(Pageable pageable) {
-         return deviceEventPortFindAll.findAllDeviceEvents(pageable)
+        return deviceEventPortFindAll.findAllDeviceEvents(pageable)
                 .stream()
                 .map(DeviceEventQueryDto::create)
                 .collect(Collectors.toList());
     }
 
     @Override
-    public List<DeviceEventQueryDto> findByCurrentUser(Pageable pageable) {
+    public List<DeviceEventQueryDto> findByCurrentUser(String owner, Pageable pageable) {
+        final var currentUser = deviceEventPortFindCurrentUser.findCurrentUser();
+        return deviceEventPortFindByOwner.findDeviceEventsByOwner(currentUser, pageable)
+                .stream()
+                .map(DeviceEventQueryDto::create)
+                .collect(Collectors.toList());
+
     }
 
+    //TODO
     @Override
     public DeviceEventQueryDto findById() {
         return null;
