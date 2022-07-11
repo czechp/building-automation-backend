24f40a3b62769cf068979ffc67c1a06ea5f5f2a8
czechp <webcoderc@gmail.com>
2022-07-11 13:55:29 +0200

    Adapter to create a new feedback event by AOP

---
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/aop/DeviceEventAopAdapterFeedback.java
@@ -0,0 +1,29 @@
+package app.web.pczportfolio.pczbuildingautomation.deviceEvent.adapter.aop;
+
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.useCase.DeviceEventUseCaseCreate;
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.domain.DeviceEventType;
+import app.web.pczportfolio.pczbuildingautomation.utilities.messaging.DeviceChannel;
+import lombok.AllArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+@AllArgsConstructor
+class DeviceEventAopAdapterFeedback {
+    private final DeviceEventUseCaseCreate deviceEventUseCaseCreate;
+
+    @AfterReturning(
+            value = "@annotation(app.web.pczportfolio.pczbuildingautomation.deviceEvent.annotation.DeviceEventFeedback)",
+            returning = "deviceChannel")
+    public void deviceEventFeedbackAdvice(JoinPoint joinPoint, Object deviceChannel) {
+        DeviceEventAopDeviceChannelMapper.castToDeviceChannel(deviceChannel)
+                .ifPresent(this::createFeedbackDeviceEvent);
+    }
+
+    private void createFeedbackDeviceEvent(DeviceChannel deviceChannel) {
+        deviceEventUseCaseCreate.createDeviceEvent(deviceChannel, DeviceEventType.FEEDBACK_FROM_DEVICE);
+    }
+}
