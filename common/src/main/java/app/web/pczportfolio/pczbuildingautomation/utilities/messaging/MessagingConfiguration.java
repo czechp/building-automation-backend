1035dbf6a2c661acb0953365fda0d2ebd23842dc
czechp <webcoderc@gmail.com>
2022-07-12 17:59:54 +0200

    Overrider default Rabbit message converter by Jackson version

--- common/src/main/java/app/web/pczportfolio/pczbuildingautomation/utilities/messaging/MessagingConfiguration.java
+++ common/src/main/java/app/web/pczportfolio/pczbuildingautomation/utilities/messaging/MessagingConfiguration.java
@@ -1,7 +1,9 @@
 package app.web.pczportfolio.pczbuildingautomation.utilities.messaging;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,5 +13,10 @@ class MessagingConfiguration {
     @Bean
     public RabbitAdmin getRabbitAdmin(@Autowired RabbitTemplate rabbitTemplate) {
         return new RabbitAdmin(rabbitTemplate);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter getMessageConverter(@Autowired ObjectMapper objectMapper) {
+        return new Jackson2JsonMessageConverter(objectMapper);
     }
 }
