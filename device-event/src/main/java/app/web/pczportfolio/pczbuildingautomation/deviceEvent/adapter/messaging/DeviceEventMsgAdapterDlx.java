d305afcd6f1b3c4cabc11f50b464a55bba08862c
czechp <webcoderc@gmail.com>
2022-07-13 00:06:27 +0200

    Invoked use case to create rejected message event in adapter

--- device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/messaging/DeviceEventMsgAdapterDlx.java
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/messaging/DeviceEventMsgAdapterDlx.java
@@ -1,27 +1,25 @@
 package app.web.pczportfolio.pczbuildingautomation.deviceEvent.adapter.messaging;
 
+import app.web.pczportfolio.pczbuildingautomation.deviceEvent.application.useCase.DeviceEventUseCaseRejectedMsg;
 import app.web.pczportfolio.pczbuildingautomation.utilities.messaging.MessagingService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 class DeviceEventMsgAdapterDlx {
     private static final String SWITCH_DEVICE_DLX_QUEUE = "q.switch-device.dlx";
     private static final String DEVICE_EVENT_LISTENER_ID = "device-event-dlx-consumer";
 
     private final MessagingService messagingService;
-
-    public DeviceEventMsgAdapterDlx(MessagingService messagingService) {
-        this.messagingService = messagingService;
-    }
+    private final DeviceEventUseCaseRejectedMsg deviceEventUseCaseRejectedMsg;
 
     @RabbitListener(id = DEVICE_EVENT_LISTENER_ID, queues = {SWITCH_DEVICE_DLX_QUEUE}, autoStartup = "false")
     void consumeRejectedMsgFromDlx(MessageFromDeviceDlx deviceChannelMsg) {
-
+        deviceEventUseCaseRejectedMsg.createEventFromRejectedMsg(deviceChannelMsg);
     }
 
     @EventListener(ApplicationReadyEvent.class)
