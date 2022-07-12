2001b778176c4fb6f24825fcaa7db3d6ddbe5eba
czechp <webcoderc@gmail.com>
2022-07-12 21:20:55 +0200

    Adapter to consumer messages from device dlx queue

--- device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/messaging/DeviceEventMsgAdapterDlx.java
+++ device-event/src/main/java/app/web/pczportfolio/pczbuildingautomation/deviceEvent/adapter/messaging/DeviceEventMsgAdapterDlx.java
@@ -1,59 +1,32 @@
 package app.web.pczportfolio.pczbuildingautomation.deviceEvent.adapter.messaging;
 
-import app.web.pczportfolio.pczbuildingautomation.utilities.messaging.DeviceChannelMsg;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import app.web.pczportfolio.pczbuildingautomation.utilities.messaging.MessagingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-public class DeviceEventMsgAdapterDlx {
+class DeviceEventMsgAdapterDlx {
     private static final String SWITCH_DEVICE_DLX_QUEUE = "q.switch-device.dlx";
-    private static final String DEVICE_EVENT_LISTENER_NAME = "device-event-dlx-consumer";
-    private final RabbitListenerEndpointRegistry rabbitListenerEndpointRegistry;
+    private static final String DEVICE_EVENT_LISTENER_ID = "device-event-dlx-consumer";
 
-    @RabbitListener(id = DEVICE_EVENT_LISTENER_NAME, queues = {SWITCH_DEVICE_DLX_QUEUE}, autoStartup = "false")
+    private final MessagingService messagingService;
+
+    public DeviceEventMsgAdapterDlx(MessagingService messagingService) {
+        this.messagingService = messagingService;
+    }
+
+    @RabbitListener(id = DEVICE_EVENT_LISTENER_ID, queues = {SWITCH_DEVICE_DLX_QUEUE}, autoStartup = "false")
     void consumeRejectedMsgFromDlx(MessageFromDeviceDlx deviceChannelMsg) {
-        System.out.println(deviceChannelMsg.getDeviceId());
-        System.out.println(deviceChannelMsg.getDeviceName());
+
     }
 
-
-    @NoArgsConstructor
-    private static class MessageFromDeviceDlx implements DeviceChannelMsg {
-        private long deviceId;
-        private String newState;
-        private String owner;
-        private String deviceName;
-        private String deviceType;
-
-        @Override
-        public long getDeviceId() {
-            return deviceId;
-        }
-
-        @Override
-        public String getNewState() {
-            return newState;
-        }
-
-        @Override
-        public String getOwner() {
-            return owner;
-        }
-
-        @Override
-        public String getDeviceName() {
-            return deviceName;
-        }
-
-        @Override
-        public String getDeviceType() {
-            return deviceType;
-        }
+    @EventListener(ApplicationReadyEvent.class)
+    void init() {
+        messagingService.listenerActivation(DEVICE_EVENT_LISTENER_ID);
     }
+
 }
