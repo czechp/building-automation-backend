de59a3607526967024a48ff514af3be2dd00a623
czechp <webcoderc@gmail.com>
2022-07-28 23:19:29 +0200

    Change signature of SecurityAuthenticator

---
+++ common/src/main/java/app/web/pczportfolio/pczbuildingautomation/configuration/security/dto/SecurityLoginQueryDto.java
@@ -0,0 +1,14 @@
+package app.web.pczportfolio.pczbuildingautomation.configuration.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SecurityLoginQueryDto {
+    private String username;
+    private String email;
+    private String role;
+}
