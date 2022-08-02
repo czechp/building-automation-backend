d785b2130b8614797a53ed599b1d9fcff5473942
czechp <webcoderc@gmail.com>
2022-08-02 20:51:38 +0200

    Expose email confirmation enpoint to all clients

--- account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/rest/AccountRestAdapterEmailConfirmation.java
+++ account/src/main/java/app/web/pczportfolio/pczbuildingautomation/account/adapter/rest/AccountRestAdapterEmailConfirmation.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/accounts/email-confirmation")
+@CrossOrigin("*")
 class AccountRestAdapterEmailConfirmation {
     private final AccountUseCaseEmailConfirmation accountUseCaseEmailConfirmation;
 
