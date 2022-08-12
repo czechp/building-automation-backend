8a81954e31fe9f012580e2c1edcf31accc3d3f33
czechp <webcoderc@gmail.com>
2022-08-12 22:08:59 +0200

    Endpoint with get request to find switch devices by location id and current user

--- location/src/main/java/app/web/pczportfolio/pczbuildingautomation/location/adapter/rest/LocationRestAdapterCreate.java
+++ location/src/main/java/app/web/pczportfolio/pczbuildingautomation/location/adapter/rest/LocationRestAdapterCreate.java
@@ -11,6 +11,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping("/api/locations")
 @AllArgsConstructor
+@CrossOrigin("*")
 class LocationRestAdapterCreate {
     private final LocationUseCaseCreate locationUseCaseCreate;
 
